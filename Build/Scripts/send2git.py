e67f45d0fc4a6246c36968e58d387283b2a9243f
defini7 <alexander.suhov@hotmail.com>
2025-02-11 17:22:11 +0300

    added a new script for building a source file from an example folder and push it to the github.io website folder

--- Build/Scripts/send2git.py
+++ Build/Scripts/send2git.py
@@ -11,28 +11,6 @@ def send(target):
 
     copy_dirs(f'../Target/{target}', f'{WEBSITE_SOURCES_PATH}/{target.lower()}')
 
-    builder = Builder()
-    
-    add = Command('git')
-    add.add_argument('add')
-    add.add_argument('--all')
-
-    commit = Command('git')
-    commit.add_argument('commit')
-    commit.add_flag('m', f'"added {target}"', True)
-
-    push = Command('git')
-    push.add_argument('push')
-    push.add_argument('origin')
-    push.add_argument('master')
-    push.add_argument('--force')
-    
-    builder.add_command(add)
-    builder.add_command(commit)
-    builder.add_command(push)
-
-    builder.execute(True, True, True, True)
-
 
 if __name__ == '__main__':
     assert len(sys.argv) > 1, 'Please provide a target'
